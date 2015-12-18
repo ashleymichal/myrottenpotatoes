5949cfeece5fa9766ced60627151b5a27f2bb77f
Ashley Michal <ashleymichal@gmail.com>
2015-12-17 20:07:24 -0600

    Added db, seed, movie model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+movies = [{ title: 'Star Wars', 							release_date: '25-Apr-1977', rating: 'PG' },
+					{ title: 'Requiem for a Dream', 		release_date: '27-Oct-2000', rating: 'R' },
+					{ title: 'Field of Dreams', 				release_date: '21-Apr-1989', rating: 'PG' },
+					{ title: 'Aladdin', 								release_date: '25-Nov-1992', rating: 'G' },
+					{ title: 'When Harry Met Sally', 		release_date: '21-Jul-1989', rating: 'R' },
+					{ title: 'The Help', 								release_date: '10-Aug-2011', rating: 'PG-13' },
+					{ title: 'Raiders of the Lost Ark', release_date: '12-Jun-1981', rating: 'PG' }
+				]
+
+movies.each do |movie|
+	Movie.create!(movie)
+end
